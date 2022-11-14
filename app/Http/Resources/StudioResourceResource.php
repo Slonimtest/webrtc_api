8e0ce9d59ff27e3444271c6023c789cd025b0ad3
Alexandr Tihonovskiy <liteforexby@gmail.com>
2022-11-14 08:11:37 +0300

    add url resource studio

--- app/Http/Resources/StudioResourceResource.php
+++ app/Http/Resources/StudioResourceResource.php
@@ -3,17 +3,25 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Requests\Request;
 
+/**
+ * Class StudioResourceResource
+ * @package App\Http\Resources
+ */
 class StudioResourceResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'url' => config('app.url') . $this->url,
+        ];
     }
 }
