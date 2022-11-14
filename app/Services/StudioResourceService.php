a6976a918d7bc5354ac21d1aa0cf4add6dfac8d0
Alexandr Tihonovskiy <liteforexby@gmail.com>
2022-11-15 00:22:18 +0300

    change folder resource

--- app/Services/StudioResourceService.php
+++ app/Services/StudioResourceService.php
@@ -48,7 +48,7 @@ class StudioResourceService extends Service
     {
         $studio = Studio::where('name', $imageData['studio_id'])->first();
 
-        $file_put = Storage::putFile('public/StudiosResources/' . $studio->id . '/' . $imageData['type'], $imageData['file']);
+        $file_put = Storage::putFile('public/studios_resources/' . $studio->id . '/' . $imageData['type'], $imageData['file']);
 
         $url = Storage::url($file_put);
 
